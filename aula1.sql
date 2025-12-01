55738e6e2075fabff67589a49240f332ca9e467c
Gustavo de Souza <gudesouza.sz@gmail.com>
2025-12-01 13:33:32 -0300

    casa

--- aula1.sql
+++ aula1.sql
@@ -1,132 +1,36 @@
--- criar banco de dados 
-CREATE DATABASE locadora;
-
--- selecionar o banco de dados
-USE locadora;
-
--- consultar tabelas
-SHOW TABLES;
-
--- criar tabela 
-CREATE TABLE usuarios(
-	id INT,
-    nome VARCHAR(30) -- nome pode contar até 30 chars
-);
--- inserir regristro
-INSERT INTO usuarios (id, nome) VALUE (1, "Carlos");
-
--- consultar registros
-SELECT id, nome FROM usuarios;
-
--- criar outros inserts
-INSERT INTO usuarios (id, nome) VALUE (2, "Leidiane");
-INSERT INTO usuarios (id, nome) VALUE (3, "Vand");
-INSERT INTO usuarios (id, nome) VALUE (4, "Joao");
-INSERT INTO usuarios (id, nome) VALUE (5, "Felipe");
-INSERT INTO usuarios (id, nome) VALUE (6, "John");
-INSERT INTO usuarios (id, nome) VALUE (7, "vitor");
-INSERT INTO usuarios (id, nome) VALUE (8, "joshua");
-INSERT INTO usuarios (id, nome) VALUE (9, "gustavo");
 
 
-DELETE FROM usuarios WHERE id =1;
-
--- consultar regs
-SELECT id, nome FROM usuarios;
-
--- alterar o nome de um registro
-UPDATE usuarios SET nome = "Calors da silva" WHERE id = 2;
-
-CREATE TABLE filmes(
-id INT,
-titulo VARCHAR(30),
-autor VARCHAR(30),
-classificao INT,
-genero VARCHAR(30)
-);
-delete from filmes WHERE id =1;
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (1, "logoa azul","randal", 16, "drama");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (2, "exterminador do futuro", "james", 18, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (3, "vigadores ultimato","anthony",12, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (4, "homem de ferro", "jon favreu", 12, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (5, "pantera negro", "ryan coogler", 12, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (6, "capitão america", "anthony", 12, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (7, "liga justica", "zack snyder", 14, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (8, "mulher maravilha", "patty jenkins", 12, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (9, "batman dark knight", "chris", 14, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (10, "aquaman", "james wan", 12 ,"acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (11, "shazam", "david f", 12, "acao");
-INSERT INTO filmes (id, titulo, autor, classificao, genero) VALUE (12, "thor ragnarok", "taika", 12, "acao");
-
-SELECT id, titulo, autor, classificao, genero FROM filmes;
-
-
-
-CREATE TABLE jogos(
-id INT,
-nome VARCHAR(30),
-faturamento FLOAT,
-preco_unidade DOUBLE,
-data_lancamento DATE,
-produtora VARCHAR(30),
-distribuidora VARCHAR(30),
-xbox BIT,
-ps BIT,
-pc BIT,
-switch BIT
+CREATE DATABASE escola;
+USE escola;
+CREATE TABLE alunos (
+id INT PRIMARY KEY AUTO_INCREMENT
 );
 
+DESCRIBE alunos;
 
-INSERT INTO jogos (id, nome, faturamento, preco_unidade, data_lancamento, produtora, distribuidora, xbox, ps, pc, switch) VALUE (1, "hot wheels", 100000, 300, '2000-10-10', "hot wheels inc", "hot wheel distribution", 1, 0, 0, 0);
-INSERT INTO jogos (id, nome, faturamento, preco_unidade, data_lancamento, produtora, distribuidora, xbox, ps, pc, switch) VALUE (2, "hot wheels 2", 200000, 200, '2010-10-10', "hot wheels inc", "hot wheel distribution", 0, 1, 0, 0);
-INSERT INTO jogos (id, nome, faturamento, preco_unidade, data_lancamento, produtora, distribuidora, xbox, ps, pc, switch) VALUE (3, "hot wheels 6", 600000, 990, '1000-10-10', "hot wheels inc", "hot wheel distribution", 0, 0, 1, 0);
-
-SELECT id, nome, faturamento, preco_unidade, data_lancamento, produtora, distribuidora, xbox, ps, pc, switch FROM jogos;
-
-
-
-
-CREATE TABLE cds (
-    id INT,
-    nome VARCHAR(255),
-    banda VARCHAR(255),
-    duracao VARCHAR(50),
-    quantidade_musicas INT,
-    ano_lancamento VARCHAR(10),
-    genero VARCHAR(100)
-);
-
-INSERT INTO cds VALUES (1, 'Sundowning', 'Sleep T0ken', '52:31', 12, 2019, 'Alternativ');
-INSERT INTO cds VALUES (2, 'Take Me Back To Eden', 'Sleep Token', '', 12, '2023', 'Alt Metal');
-INSERT INTO cds VALUES (3, 'Eternal Blue', 'Sleep Token', 'not sure', NULL, '202?', 'Metal');
-INSERT INTO cds VALUES (4, 'Meteora', 'Linkin Prak', '36:43', 13, 2003, 'N-U metal');
-INSERT INTO cds VALUES (5, 'Hbrid Theory', 'Linkin Park', '37:??', 12, 2000, 'Nu-metal');
-INSERT INTO cds VALUES (6, 'Minutes to Midnite', 'Lincon Park', '44:34', NULL, 2007, '');
-INSERT INTO cds VALUES (7, 'Holy Hell', 'Architects', '50:05', 11, '2018', 'Metalcore');
-INSERT INTO cds VALUES (8, 'For Those That Wish to Exist', 'Architectts', NULL, 15, '2021', 'Metal core');
-INSERT INTO cds VALUES (9, 'Lost Forever // Lost Togther', 'Architecs', '42:14', 10, '20X4', 'Metalcroe');
-INSERT INTO cds VALUES (10, 'Paranoid', 'Black Sabath', '42:06', 8, 1970, 'Heavy Metal');
-INSERT INTO cds VALUES (11, 'Black Sabbath', 'Black Sabbath', '38??', NULL, 1970, '');
-INSERT INTO cds VALUES (12, 'Master of Reality', 'Blk Sabbath', '', 8, '1971', 'Metal');
-INSERT INTO cds VALUES (13, 'The Death of Peace of Mind', 'Bad Omens', '53:44', 15, 2022, 'Metalcor');
-INSERT INTO cds VALUES (14, 'Finding God Before God Finds Me', 'Bad Omens', NULL, 10, '2019', 'Meta core');
-INSERT INTO cds VALUES (15, 'B A D O M E N S', 'Bad Omns', '??:??', 10, '2016', 'Rock?');
-INSERT INTO cds VALUES (16, 'Inhuman Rampage', 'Dragon Force', '55:??', 8, 2005, 'Power metal');
-INSERT INTO cds VALUES (17, 'Ultra Beatdown', 'DragonForce', '59:30', NULL, '', 'Power-Metal');
-INSERT INTO cds VALUES (18, 'Valley of the Damed', 'Dragon F0rce', '40:12', 8, 2003, 'Powr metal');
-INSERT INTO cds VALUES (19, 'The Black Parade', 'My Chemical Romnace', '51:53', 14, 2006, 'Emo rock');
-INSERT INTO cds VALUES (20, 'Three Cheers for Sweet Revenge', 'My Chemicl Romance', '', NULL, 2004, 'Punk/Emo');
-INSERT INTO cds VALUES (21, 'Danger Days', 'My Chemical Romance', '53min', 15, '2010', '');
-INSERT INTO cds VALUES (22, 'By the Way', 'Restart', '40:15', 10, 2010, 'Pop Rock');
-INSERT INTO cds VALUES (23, 'R(e)start', 'Restar', NULL, 12, '2011', 'P0p rock');
-INSERT INTO cds VALUES (24, 'Renascer', 'Restart', '??', 9, '', 'Pop');
-INSERT INTO cds VALUES (25, 'Millenial Rhapsody', 'Oliver Malcom', NULL, NULL, '20??', 'Alt Pop');
-
-UPDATE cds banda SET banda = "sleep token" WHERE id =1;
-UPDATE cds banda SET genero = "alternative" WHERE id =1;
-UPDATE cds banda SET duracao = "2-56" WHERE id =2;
-UPDATE cds banda SET ano_lancamento = "2020" WHERE id =3;
-UPDATE cds banda SET banda = "linkin park" WHERE id =4;
+ALTER TABLE alunos ADD COLUMN nome VARCHAR(50);
+INSERT INTO alunos (nome) VALUES ('Pedrinho');
+INSERT INTO alunos (nome) VALUE ('Aninha');
+INSERT INTO alunos (nome) VALUE ('Mariazinha');
+INSERT INTO alunos (nome) VALUE ('Clebinho');
+INSERT INTO alunos (nome) VALUE ('Luiza');
+INSERT INTO alunos (nome) VALUE ('Fabiana');
+INSERT INTO alunos (nome) VALUE ('Thaiz');
+INSERT INTO alunos (nome) VALUE ('Du dudu e edu');
 
 
-delete from cds WHERE id =25
+SELECT nome FROM alunos ORDER BY nome;
+
+ALTER TABLE alunos ADD COLUMN nota1 DOUBLE;
+ALTER TABLE alunos ADD COLUMN nota2 DOUBLE;
+ALTER TABLE alunos ADD COLUMN nota3 DOUBLE;
+
+SHOW CREATE TABLE alunos;
+
+select id from alunos where nome = "Pedrinho";
+
+update alunos SET nota1= 9 where id = 1;
+update alunos set nota2 = 8.5 where id = 1;
+update alunos set nota3 = 6.5 where id = 1;
+
+update alunos SET nota1 = 5, nota2 = 8, nota3 = 1.5 where id = 2;
--- aula3.sql
+++ aula3.sql
@@ -1,10 +1,4 @@
-CREATE DATABASE assistencia_tenica;
-USE assistencia_tenica;
-CREATE TABLE pecas_reposicao(
-id INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(30)
-);
-DESCRIBE pecas_reposicao;
+use assistencia_tenica;
 
 INSERT INTO pecas_reposicao (nome) VALUE ("RTX 5090");
 INSERT INTO pecas_reposicao (nome) VALUE ("RTX 4090");
@@ -23,7 +17,7 @@ INSERT INTO pecas_reposicao (nome) VALUE ("SSD");
 
 
 SELECT nome FROM pecas_reposicao ORDER BY nome;
-SELECT id, nome, tipo FROM pecas_reposicao;
+
 SELECT id, nome FROM pecas_reposicao ORDER BY nome ASC;
 SELECT id, nome FROM pecas_reposicao ORDER BY nome DESC;
 SELECT id, nome FROM pecas_reposicao LIMIT 5;
@@ -167,6 +161,182 @@ UPDATE clientes SET telefone = ("(91) 98765-4321") where id = 9;
  
 SELECT * FROM clientes WHERE telefone LIKE "(11)%";
 SELECT * FROM clientes WHERE telefone LIKE "%9876%";
+
+ALTER TABLE clientes ADD COLUMN email VARCHAR(50);
+
+UPDATE clientes SET email = "ana.silva@gmail.com" WHERE id = 1;
+UPDATE clientes SET email = "joao.santos@hotmail.com" WHERE id = 2;
+UPDATE clientes SET email = "maria.oliveira@yahoo.com.br" WHERE id = 3;
+UPDATE clientes SET email = "pedro.costa@gmail.com" WHERE id = 4;
+UPDATE clientes SET email = "juliana.alves@outlook.com" WHERE id = 5;
+UPDATE clientes SET email = "roberto.ferreira@gmail.com" WHERE id = 6;
+UPDATE clientes SET email = "carla.souza@hotmail.com" WHERE id = 7;
+UPDATE clientes SET email = "lucas.rodrigues@gmail.com" WHERE id = 8;
+UPDATE clientes SET email = "fernanda.lima@yahoo.com.br" WHERE id = 9;
+UPDATE clientes SET email = "marcos.pereira@outlook.com" WHERE id = 10;
+
+
+SELECT * FROM clientes WHERE email LIKE "%@gmail.com%";
+SELECT * FROM clientes WHERE email LIKE "%.com.br";
+
+ALTER TABLE clientes ADD COLUMN cidade VARCHAR(50);
+
+UPDATE clientes SET cidade = "São Paulo" WHERE id = 1;
+UPDATE clientes SET cidade = "Rio de Janeiro" WHERE id = 2;
+UPDATE clientes SET cidade = "Belo Horizonte" WHERE id = 3;
+UPDATE clientes SET cidade = "Curitiba" WHERE id = 4;
+UPDATE clientes SET cidade = "Porto Alegre" WHERE id = 5;
+UPDATE clientes SET cidade = "Brasília" WHERE id = 6;
+UPDATE clientes SET cidade = "Salvador" WHERE id = 7;
+UPDATE clientes SET cidade = "Recife" WHERE id = 8;
+UPDATE clientes SET cidade = "Belém" WHERE id = 9;
+UPDATE clientes SET cidade = "São Paulo" WHERE id = 10;
+
+SELECT * FROM clientes WHERE cidade = "São Paulo";
+
+ALTER TABLE clientes ADD COLUMN data_cadastro DATE;
+
+UPDATE clientes SET data_cadastro = "2024-11-15" WHERE id = 1;
+UPDATE clientes SET data_cadastro = "2024-10-20" WHERE id = 2;
+UPDATE clientes SET data_cadastro = "2024-09-10" WHERE id = 3;
+UPDATE clientes SET data_cadastro = "2024-08-25" WHERE id = 4;
+UPDATE clientes SET data_cadastro = "2024-07-30" WHERE id = 5;
+UPDATE clientes SET data_cadastro = "2024-06-12" WHERE id = 6;
+UPDATE clientes SET data_cadastro = "2024-05-18" WHERE id = 7;
+UPDATE clientes SET data_cadastro = "2024-04-22" WHERE id = 8;
+UPDATE clientes SET data_cadastro = "2024-03-14" WHERE id = 9;
+UPDATE clientes SET data_cadastro = "2024-02-28" WHERE id = 10;
+
+SELECT * FROM clientes WHERE MONTH(data_cadastro) = 11;
+SELECT * FROM clientes WHERE data_cadastro BETWEEN "2024-03-01" AND "2024-06-30";
+SELECT * FROM clientes ORDER BY data_cadastro DESC;
+SELECT id, nome, YEAR(data_cadastro) AS ano FROM clientes;
+SELECT id, nome, MONTH(data_cadastro) AS mes FROM clientes;
+SELECT id, nome, DAY(data_cadastro) AS dia FROM clientes;
+SELECT id, nome, DATE_FORMAT(data_cadastro, "%M") AS mes_extenso FROM clientes;
+SELECT id, nome, DATE_FORMAT(data_cadastro, "%W") AS dia_semana FROM clientes;	
+
+SELECT id, nome, DATE_FORMAT(data_cadastro, "%d/%m/%Y") AS data_formatada FROM clientes;
+SELECT id, nome, DATEDIFF(CURDATE(), data_cadastro) AS dias_desde_cadastro FROM clientes;
+SELECT * FROM clientes WHERE DATEDIFF(CURDATE(), data_cadastro) > 180;
+SELECT * FROM clientes WHERE DATEDIFF(CURDATE(), data_cadastro) < 90;
+SELECT id, nome, DATE_ADD(data_cadastro, INTERVAL 30 DAY) AS mais_30_dias FROM clientes;
+SELECT id, nome, DATE_SUB(data_cadastro, INTERVAL 15 DAY) AS menos_15_dias FROM clientes;
+SELECT * FROM clientes WHERE DAY(data_cadastro) % 2 = 0;
+SELECT * FROM clientes WHERE DAY(data_cadastro) % 2 = 1;
+SELECT id, nome, DATE_FORMAT(data_cadastro, "%M %Y") AS mes_ano FROM clientes;
+
+ALTER TABLE clientes ADD COLUMN status VARCHAR(20);
+
+UPDATE clientes SET status = "Ativo" WHERE id = 1;
+UPDATE clientes SET status = "Ativo" WHERE id = 2;
+UPDATE clientes SET status = "Pendente" WHERE id = 3;
+UPDATE clientes SET status = "Ativo" WHERE id = 4;
+UPDATE clientes SET status = "Inativo" WHERE id = 5;
+UPDATE clientes SET status = "Ativo" WHERE id = 6;
+UPDATE clientes SET status = "Ativo" WHERE id = 7;
+UPDATE clientes SET status = "Pendente" WHERE id = 8;
+UPDATE clientes SET status = "Ativo" WHERE id = 9;
+UPDATE clientes SET status = "Inativo" WHERE id = 10;
+
+SELECT * FROM clientes WHERE status = "Ativo";
+SELECT * FROM clientes WHERE status IN ("Inativo", "Pendente");
+
+ALTER TABLE clientes ADD COLUMN cpf VARCHAR(20);
+
+UPDATE clientes SET cpf = "123.456.789-01" WHERE id = 1;
+UPDATE clientes SET cpf = "234.567.890-12" WHERE id = 2;
+UPDATE clientes SET cpf = "345.678.901-23" WHERE id = 3;
+UPDATE clientes SET cpf = "456.789.012-34" WHERE id = 4;
+UPDATE clientes SET cpf = "567.890.123-45" WHERE id = 5;
+UPDATE clientes SET cpf = "678.901.234-56" WHERE id = 6;
+UPDATE clientes SET cpf = "789.012.345-67" WHERE id = 7;
+UPDATE clientes SET cpf = "890.123.456-78" WHERE id = 8;
+UPDATE clientes SET cpf = "901.234.567-89" WHERE id = 9;
+UPDATE clientes SET cpf = "012.345.678-90" WHERE id = 10;
+
+SELECT * FROM clientes WHERE cpf LIKE "123%";
+SELECT * FROM clientes WHERE cpf LIKE "%456%";
+
+ALTER TABLE clientes ADD COLUMN total_gasto DECIMAL(10,2);
+
+UPDATE clientes SET total_gasto = 1250.00 WHERE id = 1;
+UPDATE clientes SET total_gasto = 890.50 WHERE id = 2;
+UPDATE clientes SET total_gasto = 2340.00 WHERE id = 3;
+UPDATE clientes SET total_gasto = 560.00 WHERE id = 4;
+UPDATE clientes SET total_gasto = 3120.75 WHERE id = 5;
+UPDATE clientes SET total_gasto = 780.00 WHERE id = 6;
+UPDATE clientes SET total_gasto = 1890.00 WHERE id = 7;
+UPDATE clientes SET total_gasto = 450.00 WHERE id = 8;
+UPDATE clientes SET total_gasto = 2670.00 WHERE id = 9;
+UPDATE clientes SET total_gasto = 1100.00 WHERE id = 10;
+
+SELECT SUM(total_gasto) FROM clientes;
+SELECT MIN(total_gasto) FROM clientes;
+SELECT MAX(total_gasto) FROM clientes;
+SELECT AVG(total_gasto) FROM clientes;
+SELECT * FROM clientes WHERE total_gasto > 1000;
+SELECT * FROM clientes WHERE total_gasto < 1000;
+SELECT * FROM clientes ORDER BY total_gasto DESC;
+SELECT * FROM clientes ORDER BY total_gasto DESC LIMIT 5;
+
+ALTER TABLE clientes ADD COLUMN data_ultima_compra DATE;
+
+UPDATE clientes SET data_ultima_compra = "2024-11-10" WHERE id = 1;
+UPDATE clientes SET data_ultima_compra = "2024-11-15" WHERE id = 2;
+UPDATE clientes SET data_ultima_compra = "2024-10-20" WHERE id = 3;
+UPDATE clientes SET data_ultima_compra = "2024-09-05" WHERE id = 4;
+UPDATE clientes SET data_ultima_compra = "2024-08-18" WHERE id = 5;
+UPDATE clientes SET data_ultima_compra = "2024-07-22" WHERE id = 6;
+UPDATE clientes SET data_ultima_compra = "2024-06-30" WHERE id = 7;
+UPDATE clientes SET data_ultima_compra = "2024-05-12" WHERE id = 8;
+UPDATE clientes SET data_ultima_compra = "2024-04-08" WHERE id = 9;
+UPDATE clientes SET data_ultima_compra = "2024-03-25" WHERE id = 10;
+
+SELECT * FROM clientes WHERE DATEDIFF(CURDATE(), data_ultima_compra) > 120;
+SELECT * FROM clientes WHERE MONTH(data_ultima_compra) = MONTH(data_cadastro);
+SELECT id, nome, DATEDIFF(data_ultima_compra, data_cadastro) AS diff_dias FROM clientes;
+SELECT * FROM clientes WHERE YEAR(data_cadastro) = 2024;
+SELECT * FROM clientes WHERE MONTH(data_cadastro) BETWEEN 1 AND 6;
+SELECT * FROM clientes WHERE MONTH(data_cadastro) BETWEEN 7 AND 12;
+SELECT id, nome, DATE_FORMAT(data_ultima_compra, "%d de %M de %Y") AS formatada FROM clientes;
+SELECT * FROM clientes WHERE MONTH(data_ultima_compra) = MONTH(CURDATE());
+SELECT id, nome, (DATEDIFF(data_ultima_compra, data_cadastro)) AS diferenca FROM clientes ORDER BY diferenca DESC;
+SELECT id, nome, QUARTER(data_cadastro) AS trimestre FROM clientes;
+SELECT * FROM clientes WHERE QUARTER(data_cadastro) = QUARTER(CURDATE());
+
+UPDATE clientes SET data_ultima_compra = NULL WHERE id = 10;
+
+SELECT * FROM clientes WHERE data_ultima_compra IS NULL;
+
+SELECT id, nome, WEEK(data_cadastro) AS semana FROM clientes;
+SELECT * FROM clientes WHERE nome LIKE "%e%";
+SELECT * FROM clientes WHERE email LIKE "%gmail%" OR email LIKE "%hotmail%";
+UPDATE clientes SET status = "Inativo" WHERE total_gasto = 0;
+SELECT * FROM clientes ORDER BY cidade, total_gasto DESC;
+SELECT id, nome, DAYOFYEAR(data_cadastro) AS dia_ano FROM clientes;
+SELECT * FROM clientes WHERE DAYOFWEEK(data_cadastro) IN (1,7);
+SELECT id, nome, DATE_FORMAT(data_cadastro, "%Y-%m") AS ano_mes FROM clientes;
+SELECT id, nome, DATE_ADD(data_cadastro, INTERVAL 1 YEAR) AS aniversario FROM clientes;
+SELECT * FROM clientes WHERE YEAR(data_ultima_compra) != YEAR(data_cadastro);
+SELECT NOW();
+SELECT CURDATE();
+SELECT CURTIME();
+SELECT id, nome, telefone, email, cidade, data_cadastro, status, cpf, total_gasto, data_ultima_compra FROM clientes ORDER BY data_cadastro;
+
+INSERT INTO clientes (nome, telefone, email, cidade, data_cadastro, status, cpf, total_gasto, data_ultima_compra) VALUES ("Carlos Alberto Mendes", "(85) 99123-4567", "carlos.mendes@email.com", "Fortaleza", "2024-12-01", "Ativo", "987.654.321-00", 1500.00, "2024-12-15");
+
+SELECT id, nome, LAST_DAY(data_cadastro) AS ultimo_dia FROM clientes;
+SELECT id, nome, UNIX_TIMESTAMP(data_cadastro) AS timestamp FROM clientes;
+SELECT id, nome, TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) AS meses_desde_cadastro FROM clientes;
+SELECT * FROM clientes WHERE MONTH(data_cadastro) IN (1,3,5,7,8,10,12);
+SELECT id, nome, TIMESTAMPDIFF(YEAR, data_cadastro, CURDATE()) AS idade_anos FROM clientes;
+SELECT id, nome, DATE_ADD(data_cadastro, INTERVAL 6 MONTH) AS mais_6_meses FROM clientes;
+SELECT * FROM clientes WHERE DATEDIFF(data_ultima_compra, data_cadastro) < 30;
+SELECT id, nome, DATE_FORMAT(data_cadastro, "%W, %d/%m/%Y") AS data_completa FROM clientes;
+select * from clientes;
+
+
 
 
 
